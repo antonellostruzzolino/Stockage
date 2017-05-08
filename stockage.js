3a0fe84415ad7cacb4f171723d5e5eb3a2d98eaf
Antonello Struzzolino <strant@computacenter.ch>
2017-05-08 16:43:35 +0200

    complete

---
+++ stockage.js
@@ -0,0 +1,55 @@
+(function(factory) {
+
+    window.Stockage = {
+        Local: new factory(localStorage),
+        Session: new factory(sessionStorage)
+    };
+
+}) (function(container) {
+
+    var check = 'Stockage';
+
+    try {
+        container.setItem(check, check);
+        container.removeItem(check);
+    } catch (e) {
+        console.log('Stockage is not supported!');
+        return {};
+    }
+
+    return {
+
+        get: function(key) {
+            return JSON.parse(container.getItem(key));
+        },
+
+        set: function(key, value) {
+            container.setItem(key, JSON.stringify(value));
+        },
+
+        has: function(key) {
+            var result = this.get(key);
+            return (result !== null || typeof result !== 'undefined') ? true : false;
+        },
+
+        list: function() {
+
+            var list = {};
+            for (var i in container) {
+                var item = this.get(i);
+                if (item) {
+                    list[i] = item;
+                }
+            }
+            return list;
+        },
+
+        remove: function(key) {
+            container.removeItem(key)
+        },
+
+        clear: function() {
+            container.clear()
+        }
+    }
+});
